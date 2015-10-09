7798f2ac5f9c7714f2e0201d3f42a5aacd4b8040
Bartosz Boron <bartosz.boron@senacor.com>
2015-10-09 14:18:01 +0200

    using mongo collectio now

--- simple-todos.js
+++ simple-todos.js
@@ -1,9 +1,28 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
     Template.body.helpers({
-        tasks:[
-            { text : "This is task 1" },
-            { text : "This is task 2" },
-            { text : "This is task 3" }
-        ]
+        tasks: function () {
+            return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+    });
+
+    Template.body.events({
+        "submit .new-task": function (event) {
+            // prevent default browser form submit
+            event.preventDefault();
+
+            // get valut from form element
+            var text = event.target.text.value;
+
+            // insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date()
+            });
+
+            // clear form
+            event.target.text.value = "";
+        }
     });
 }
